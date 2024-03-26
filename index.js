261bd34cc4380916ff446895647a876c0b3f267d
BRISSETLeo <leo.brisset@etu.univ-orleans.fr>
2024-03-26 14:16:35 +0100

    finission de la pagination

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let nbCharacters = 0;
 let nbPages = 1;
 let currentPage = 1;
 
-// Définition des classes
 class Character {
   constructor(name, role, provenance, description, equipements, abilities, image) {
     this.name = name;
@@ -16,29 +15,15 @@ class Character {
   }
 }
 
-// Fonction pour charger les personnages depuis un fichier JSON
-async function loadCharacters() {
+async function loadCharacters(recherche, start, end) {
   try {
     const response = await fetch('./static/json/characters.json');
     const data = await response.json();
-    return data.characters.map(characterData => {
-      const { name, role, provenance, description, equipements, abilities, image } = characterData;
-      return new Character(name, role, provenance, description, equipements, abilities, image);
-    });
-  } catch (error) {
-    console.error('Erreur lors du chargement des personnages :', error);
-    return [];
-  }
-}
-
-async function loadCharactersByStartAndEnd(start, end) {
-  try {
-    const response = await fetch('./static/json/characters.json');
-    const data = await response.json();
-    if (data.characters.length < end) {
-      end = data.characters.length;
+    var characters = data.characters.filter(character => character.name.toLowerCase().includes(recherche.toLowerCase()));
+    if (characters.length < end) {
+      end = characters.length;
     }
-    const charactersData = data.characters.slice(start, end);
+    const charactersData = (start === undefined || end === undefined) ? characters : characters.slice(start, end);
     return charactersData.map(characterData => {
       const { name, role, provenance, description, equipements, abilities, image } = characterData;
       return new Character(name, role, provenance, description, equipements, abilities, image);
@@ -50,43 +35,43 @@ async function loadCharactersByStartAndEnd(start, end) {
 }
 
 async function getNotes() {
-    const response = await fetch('http://localhost:3000/character');
-    if (!response.ok) {
-        throw new Error('Erreur de récupération des données.');
-    }
-    return await response.json();
+  const response = await fetch('http://localhost:3000/character');
+  if (!response.ok) {
+    throw new Error('Erreur de récupération des données.');
+  }
+  return await response.json();
 }
 
 async function noter(characterData) {
-    var maNote = prompt("Entrez votre note pour ce personnage");
-    if(maNote === null || maNote === "") {
-        return;
-    }
-    else if(isNaN(maNote)) {
-        alert("Veuillez entrer un nombre");
-        return;
-    }
-    if(maNote < 0 || maNote > 10) {
-        alert("Veuillez entrer une note entre 0 et 10");
-        return;
-    }
-    const notes = await getNotes();
-    if(notes[characterData] === undefined) {
-        notes[characterData] = [parseInt(maNote)];
-    }else{
-      notes[characterData].push(parseInt(maNote));
-    }
-    const response = await fetch('http://localhost:3000/character', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(notes)
-    });
-    if (!response.ok) {
-        throw new Error('Erreur de sauvegarde des données.');
-    }
-    console.log('Note ajoutée');
+  var maNote = prompt("Entrez votre note pour ce personnage");
+  if (maNote === null || maNote === "") {
+    return;
+  }
+  else if (isNaN(maNote)) {
+    alert("Veuillez entrer un nombre");
+    return;
+  }
+  if (maNote < 0 || maNote > 10) {
+    alert("Veuillez entrer une note entre 0 et 10");
+    return;
+  }
+  const notes = await getNotes();
+  if (notes[characterData] === undefined) {
+    notes[characterData] = [parseInt(maNote)];
+  } else {
+    notes[characterData].push(parseInt(maNote));
+  }
+  const response = await fetch('http://localhost:3000/character', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(notes)
+  });
+  if (!response.ok) {
+    throw new Error('Erreur de sauvegarde des données.');
+  }
+  console.log('Note ajoutée');
 }
 
 async function displayCharacterDetail(character) {
@@ -118,35 +103,35 @@ async function displayCharacterDetail(character) {
   characterDetail.appendChild(noteButton);
 }
 
-document.getElementById('search').addEventListener('keyup', function(event) {
+document.getElementById('search').addEventListener('keyup', async function (event) {
+  currentPage = 1;
   const search = event.target.value.toLowerCase();
-  const characterList = document.getElementById('character-list');
-  const characters = characterList.getElementsByTagName('li');
-  Array.from(characters).forEach(character => {
-    const name = character.textContent.toLowerCase();
-    if (name.indexOf(search) === -1) {
-      character.style.display = 'none';
-    } else {
-      character.style.display = 'block';
-    }
-  });
+  const characterList = await loadCharacters(search);
+  document.getElementById('character-list').innerHTML = '';
+  nbCharacters = characterList.length;
+  nbPages = Math.ceil(nbCharacters / charactersPerPage);
+  document.getElementById('pagination').innerHTML = '';
+  for (let i = 1; i <= nbPages; i++) {
+    ajouterBoutonPageSupp(i);
+  }
+  displayCharactersByPage(search);
 });
 
 function displayCharacter(character) {
-    const characterList = document.getElementById('character-list');
-    const divEleme = document.createElement('div');
-    const imgElement = document.createElement('img');
-    imgElement.src = character.image;
-    const characterElement = document.createElement('p');
-    characterElement.textContent = character.name;
-    divEleme.addEventListener('click', () => displayCharacterDetail(character));
-    divEleme.appendChild(imgElement);
-    divEleme.appendChild(characterElement);
-    characterList.appendChild(divEleme);
+  const characterList = document.getElementById('character-list');
+  const divEleme = document.createElement('div');
+  const imgElement = document.createElement('img');
+  imgElement.src = character.image;
+  const characterElement = document.createElement('p');
+  characterElement.textContent = character.name;
+  divEleme.addEventListener('click', () => displayCharacterDetail(character));
+  divEleme.appendChild(imgElement);
+  divEleme.appendChild(characterElement);
+  characterList.appendChild(divEleme);
 }
 
 async function initializeApp() {
-  const characters = await loadCharacters();
+  const characters = await loadCharacters("");
   document.getElementById('character-list').innerHTML = '';
   nbCharacters = characters.length;
   nbPages = Math.ceil(nbCharacters / charactersPerPage);
@@ -154,7 +139,7 @@ async function initializeApp() {
   for (let i = 1; i <= nbPages; i++) {
     ajouterBoutonPageSupp(i);
   }
-  displayCharactersByPage();
+  displayCharactersByPage("");
 }
 
 function ajouterBoutonPageSupp(numPage) {
@@ -163,13 +148,13 @@ function ajouterBoutonPageSupp(numPage) {
   boutonPage.addEventListener('click', () => {
     currentPage = numPage;
     document.getElementById('character-list').innerHTML = '';
-    displayCharactersByPage();
+    displayCharactersByPage(document.getElementById('search').value || '');
   });
   document.getElementById('pagination').appendChild(boutonPage);
 }
 
-async function displayCharactersByPage() {
-  const characters = await loadCharactersByStartAndEnd((currentPage - 1) * charactersPerPage, currentPage * charactersPerPage);
+async function displayCharactersByPage(recherche) {
+  const characters = await loadCharacters(recherche, (currentPage - 1) * charactersPerPage, currentPage * charactersPerPage);
   characters.forEach(displayCharacter);
 }
 
